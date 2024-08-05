0effed5a5eb51b042f81f633dd98ec0c421ed97f
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-08-05 16:38:08 +0800

    Add user validation schemas

---
+++ src/validations/user-validation.js
@@ -0,0 +1,27 @@
+import Joi from "joi";
+
+const registerUserValidation = Joi.object({
+    username: Joi.string().max(100).required(),
+    password: Joi.string().max(100).required(),
+    email: Joi.string().max(100).required()
+});
+
+const loginUserValidation = Joi.object({
+    username: Joi.string().max(100).required(),
+    password: Joi.string().max(100).required()
+});
+
+const updateUserValidation = Joi.object({
+    username: Joi.string().max(100).required(),
+    password: Joi.string().max(100).optional(),
+    email: Joi.string().max(100).optional()
+})
+
+const getUserValidation = Joi.string().max(100).required();
+
+export {
+    registerUserValidation,
+    loginUserValidation,
+    getUserValidation,
+    updateUserValidation
+}
