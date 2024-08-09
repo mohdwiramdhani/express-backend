14d2c674246b12e9b4b8d3bb6b30b4937ce05a84
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-08-10 02:20:19 +0800

    Refactor auth middleware and add admin middleware to API

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
-import { prismaClient } from "../config/database.js"; // Pastikan Anda mengimpor prismaClient jika perlu
+import { logger } from "../config/logging.js";
 
-export const authMiddleware = async (req, res, next) => {
+export const authMiddleware = (req, res, next) => {
     try {
         const authHeader = req.header('Authorization');
 
@@ -11,32 +11,17 @@ export const authMiddleware = async (req, res, next) => {
 
         const token = authHeader.replace('Bearer ', '');
 
-        jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
+        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
             if (err) {
                 return res.status(401).json({ errors: "Unauthorized" });
             }
 
-            const userId = decoded.id;
-
-            const user = await prismaClient.user.findUnique({
-                where: { id: userId },
-                select: { id: true, role: { select: { name: true } } }
-            });
-
-            if (!user) {
-                return res.status(401).json({ errors: "Unauthorized" });
-            }
-
-            req.user = { id: user.id, role: user.role.name };
-
-            if (req.path.startsWith('/users/') && req.method === 'DELETE' && req.user.role !== 'admin') {
-                return res.status(403).json({ errors: 'Forbidden' });
-            }
+            req.user = { id: decoded.id, role: decoded.role };
 
             next();
         });
     } catch (error) {
-        console.error("Auth Middleware Error:", error);
+        logger.error(`Auth Middleware Error: ${error.message}`);
         return res.status(500).json({ errors: "Internal Server Error" });
     }
 };
--- src/routes/v1/api.js
+++ src/routes/v1/api.js
@@ -2,14 +2,16 @@ import express from "express";
 import userController from "../../controllers/user-controller.js";
 import { authMiddleware } from "../../middlewares/auth-middleware.js";
 import { registerRateLimiter } from '../../config/rate-limit.js';
+import { adminMiddleware } from "../../middlewares/admin-middleware.js";
 
 const userRouter = new express.Router();
 userRouter.use(authMiddleware);
 
 userRouter.get('/users/current', userController.get);
 userRouter.patch('/users/current', userController.update);
-userRouter.post('/users/register', registerRateLimiter, userController.registerStaff);
-userRouter.delete('/users/:id', userController.deleteStaff);
+
+userRouter.post('/users/register', adminMiddleware, registerRateLimiter, userController.registerStaff);
+userRouter.delete('/users/:id', adminMiddleware, userController.deleteStaff);
 
 export {
     userRouter
