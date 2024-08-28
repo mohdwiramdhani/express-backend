7b8ab0a6f5d20bf748dd21a7f1c259f5a1ea5ee8
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-08-28 20:52:32 +0800

    Add seeder for register user admin

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 import { logger } from "../src/config/logging.js";
 
 const prisma = new PrismaClient();
@@ -28,9 +29,42 @@ const createRoles = async () => {
     }
 };
 
+const createAdminUser = async () => {
+    const adminUsername = "admin";
+    const adminPassword = "12345";
+    const hashedPassword = await bcrypt.hash(adminPassword, 10);
+
+    const countAdmin = await prisma.user.count({
+        where: {
+            username: adminUsername
+        }
+    });
+
+    if (countAdmin === 0) {
+        const adminUser = await prisma.user.create({
+            data: {
+                username: adminUsername,
+                password: hashedPassword,
+                roleId: 1,
+            }
+        });
+
+        await prisma.profile.create({
+            data: {
+                userId: adminUser.id,
+            }
+        });
+
+        logger.info(`Admin user created successfully with username: ${adminUsername}`);
+    } else {
+        logger.info(`Admin user with username ${adminUsername} already exists.`);
+    }
+};
+
 createRoles()
+    .then(createAdminUser)
     .catch((e) => {
-        logger.error('Error creating roles:', e);
+        logger.error('Error creating roles or admin user:', e);
         prisma.$disconnect();
     })
     .finally(() => {
