8eff6a894bfa637b7ec26c915c2552ed5bc9c264
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-08-28 20:53:31 +0800

    Remove create profile validation

--- src/validations/profile-validation.js
+++ src/validations/profile-validation.js
@@ -1,25 +1,16 @@
 import Joi from "joi";
 
-const createProfileValidation = Joi.object({
-    fullName: Joi.string().max(255).required(),
-    nik: Joi.string().length(16).optional(),
-    phoneNumber: Joi.string().max(20).optional(),
-    address: Joi.string().optional(),
-    dateOfBirth: Joi.date().optional(),
-});
-
 const getProfileValidation = Joi.number().positive().required();
 
 const updateProfileValidation = Joi.object({
     fullName: Joi.string().max(255).optional(),
-    nik: Joi.string().length(16).optional(),
+    nik: Joi.string().length(16).pattern(/^[0-9]+$/).optional(),
     phoneNumber: Joi.string().max(20).optional(),
     address: Joi.string().optional(),
     dateOfBirth: Joi.date().optional(),
 })
 
 export {
-    createProfileValidation,
     getProfileValidation,
     updateProfileValidation
 }
