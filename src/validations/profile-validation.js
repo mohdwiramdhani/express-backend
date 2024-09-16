9c567838f6e3b7c869ccb33d5a7398fab095835c
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-09-16 23:52:38 +0800

    fix profile validation

--- src/validations/profile-validation.js
+++ src/validations/profile-validation.js
@@ -3,11 +3,11 @@ import Joi from "joi";
 const getProfileValidation = Joi.number().positive().required();
 
 const updateProfileValidation = Joi.object({
-    fullName: Joi.string().max(255).optional(),
-    nik: Joi.string().length(16).pattern(/^[0-9]+$/).optional(),
-    phoneNumber: Joi.string().max(20).optional(),
-    address: Joi.string().optional(),
-    dateOfBirth: Joi.date().optional(),
+    fullName: Joi.string().max(255).allow(null, ''),
+    nik: Joi.string().length(16).pattern(/^[0-9]+$/).allow(null, ''),
+    phoneNumber: Joi.string().max(20).allow(null, ''),
+    address: Joi.string().allow(null, ''),
+    dateOfBirth: Joi.date().allow(null, ''),
 })
 
 export {
